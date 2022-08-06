8651723ae7bc5bf3fc30bb1e6bea2c12aaa1b942
Pushkar Sharma <pushkar@skeps.com>
2022-08-07 02:17:50 +0530

    remove async error handler import

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,12 +2,11 @@ const express = require('express');
 const schemas = require('../utils/validationSchema');
 const users = require('../controllers/user.controller.js');
 const requestValidator = require('../middlewares/requestValidator');
-const aeh = require('../middlewares/asyncErrorHandler');
 
 const router = new express.Router();
 
 router.post('/food-entry', requestValidator(schemas.foodEntry), users.createFoodEntry);
 router.get('/:userId/food-entry', users.fetchFoodEntries);
-router.patch('/update-calorie-limit', requestValidator(schemas.updateCalorieLimit), aeh(users.updateCalorieLimit));
+router.patch('/update-calorie-limit', requestValidator(schemas.updateCalorieLimit), users.updateCalorieLimit);
 
 module.exports = router;
